e4063bbeb38c644b1f2dca6568db8828deb9c5e3
r41l1m4 <rai.lima17@gmail.com>
2025-07-06 14:03:33 -0300

    Initial commit

---
+++ src/main/java/com/ironia/loginauthapi/LoginAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.ironia.loginauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAuthApiApplication.class, args);
+	}
+
+}
