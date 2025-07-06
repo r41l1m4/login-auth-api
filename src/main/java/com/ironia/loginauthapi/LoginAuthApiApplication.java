0a841c95e8910707d9c8510839ebeb35504f6775
r41l1m4 <rai.lima17@gmail.com>
2025-07-06 14:08:45 -0300

    Initial configs

--- src/main/java/com/ironia/loginauthapi/LoginAuthApiApplication.java
+++ src/main/java/com/ironia/loginauthapi/LoginAuthApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoginAuthApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoginAuthApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LoginAuthApiApplication.class, args);
+    }
 
 }
